ad52383c6380a110e31fa1ba306b66e9e1f51dfd
ultraflame4 <ultraflame4@gmail.com>
2021-05-21 22:49:34 +0800

    - did channel listing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function createWindow(){
 
     win = new BrowserWindow(
         {
-            width:800,
+            width:1000,
             height:600,
             webPreferences: {
                 preload: path.join(__dirname, 'preload.js')
@@ -26,9 +26,42 @@ function createWindow(){
 
     ipcMain.on("LoginRequest",login_request_callback)
 
-    ipcMain.on("requestOpenGuildContent",(e,guild_id)=>{
 
-        win.webContents.send("openGuildContent",guild_id); // Send back a signal with required data
+
+    ipcMain.on("requestOpenGuildContent",(e,guild_id)=>{
+        if (guild_id==="home"){ //check if user is opening the "home"
+            e.sender.send("openHomeContent"); // if opening home, send required data for home
+            return
+        }
+
+        client.guilds.fetch(guild_id).then((guild)=>{
+            e.sender.send("openGuildContent",guild.id,guild.name);
+            // Send back a signal with required data
+
+
+        })
+
+    })
+
+    ipcMain.on("populateGuildChannel",(e,guildId)=> {
+        console.log("populating...")
+        // Loop through guild channels and add them tto sidebar
+        client.guilds.fetch(guildId).then((guild)=> {
+            for (const c of guild.channels.cache) {
+
+                let channel = c[1]
+                // Skip category and voice channels for now. todo add support for categories and voice
+                if (channel.type === "category" || channel.type === "voice") {
+                    continue
+                }
+
+                // skip deleted channels
+                if (channel.deleted === true) {
+                    continue
+                }
+                e.sender.send("addGuildChannel", guild.id, channel.id, channel.name)
+            }
+        })
     })
 
 }
@@ -61,8 +94,6 @@ app.on("before-quit", () => {
 function login_request_callback() {
     win.webContents.send("loggedin",true); //prevent user from clicking button again
 
-
-
     prompt({
         title: "Discord Bot Login",
         label: "Token: ",
@@ -87,7 +118,7 @@ function discordLogin(token){
     if (token===null){return}
 
     client.login(token).catch((r) => {
-        console.log(r)
+
         win.webContents.send("loggedin", false)
     });
 
@@ -95,9 +126,8 @@ function discordLogin(token){
 
 
 client.on('ready', ()=>{
-    win.webContents.send("botready",client.user.username)
+    win.webContents.send("botready",client.user.username,client.user.avatarURL(),client.user.tag)
 
-    console.log(client.guilds.cache)
     for (const g of client.guilds.cache) {
         win.webContents.send("addGuild",g[1].name,g[1].id,g[1].iconURL())
     }
--- preload.js
+++ preload.js
@@ -2,6 +2,9 @@ const { ipcRenderer } = require('electron')
 const Discord = require("discord.js")
 
 
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#botTokenLogin').addEventListener('click', () => {
         ipcRenderer.send("LoginRequest")
@@ -12,23 +15,51 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
-    ipcRenderer.on("botready",(e,name,guilds)=>{
+    ipcRenderer.on("botready",(e,name,avatarUrl,tag)=>{
         document.getElementById("botstatushere").textContent = "Ready";
         document.getElementById("botnamehere").textContent = name;
+        document.getElementById("profile-area-img").src=avatarUrl
+        document.getElementById("profile-area-user-name").textContent = name;
+        document.getElementById("profile-area-user-id").textContent = tag;
+    })
+
+
+    ipcRenderer.on("openHomeContent",()=>{
+        // When in home, no member list, so hide it
+        document.getElementById("memberlist-panel").style.display="none"
+        document.getElementById("sidebar-header").style.display="none"
+    })
+
+
+
+    ipcRenderer.on("openGuildContent",(e,guildId,guildName)=>{
+        // renable member list.
+        document.getElementById("memberlist-panel").style.display="flex"
+        let sidebar_header = document.getElementById("sidebar-header")
+        sidebar_header.style.display="flex"
+        sidebar_header.textContent = guildName
+        // clear guild channels
+        let channelContainer = document.getElementById("sidebar-channel-container")
+
+        channelContainer.innerHTML=""
+        // populate channels
+        console.log("populate")
+        ipcRenderer.send("populateGuildChannel",guildId)
 
     })
 
-    ipcRenderer.on("openGuildContent",(e,guildId)=>{
-        //todo
-    })
-
+    // Add click event listener for home
+    document.getElementById("guild-home").addEventListener("click", () => {
+        ipcRenderer.send("requestOpenGuildContent", "home")
+        // Send signal to main process, main process thens sends back another signal with required data
+    });
 
     ipcRenderer.on("addGuild",(e,name,guildId,imageUrl)=>{
         let guildlist = document.getElementById("guildlist")
         let guildItemContainer = document.createElement("div")
         guildItemContainer.className = "guildListItemContainer row tooltip"
         guildItemContainer.dataset.tooltip = name
-        guildItemContainer.dataset.guildId = guildId
+        guildItemContainer.dataset.guildid = guildId
 
         guildItemContainer.addEventListener("click", () => {
             ipcRenderer.send("requestOpenGuildContent", guildId)
@@ -46,8 +77,23 @@ window.addEventListener('DOMContentLoaded', () => {
         guildlist.append(guildItemContainer)
     })
 
+    ipcRenderer.on("addGuildChannel", (e, guildId, channelId, channelName) => {
+        let channel_container = document.getElementById("sidebar-channel-container")
+        let channel_block = document.createElement("div")
+        let channel_label = document.createElement("label")
+        channel_label.textContent="#"+channelName
+        channel_label.style.fontSize="inherit"
+        channel_block.append(channel_label)
+        channel_block.className = "channel-container-item row"
+        channel_block.dataset.guildId=guildId
+        channel_block.dataset.channelId=channelId
+        channel_container.append(channel_block)
+    });
 
 })
+
+
+
 
 
 document.addEventListener("keydown", function (e) {
