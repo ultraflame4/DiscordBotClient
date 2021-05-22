d6d41e254ca92890c573df3362653febbe55e677
ultraflame4 <ultraflame4@gmail.com>
2021-05-22 20:20:42 +0800

    - can read message history, somehow managed to fix overflow

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const {app,BrowserWindow,ipcMain} = require("electron")
+const {app, BrowserWindow, ipcMain} = require("electron")
 const prompt = require('electron-prompt');
 const path = require('path')
+const moment = require("moment")
+
+
+
+
+
+
+
 
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var win = null;
 
-function createWindow(){
+function createWindow() {
 
     win = new BrowserWindow(
         {
-            width:1000,
-            height:600,
+            width: 1000,
+            height: 600,
             webPreferences: {
                 preload: path.join(__dirname, 'preload.js')
             }
@@ -22,20 +30,23 @@ function createWindow(){
 
     win.loadFile("index.html")
 
-    ipcMain.on("devtools",()=>{win.webContents.toggleDevTools()})
+    ipcMain.on("devtools", () => {
+        win.webContents.toggleDevTools()
+    })
 
-    ipcMain.on("LoginRequest",login_request_callback)
+    ipcMain.on("LoginRequest", login_request_callback)
 
 
+    // Requests to open contet listeners
 
-    ipcMain.on("requestOpenGuildContent",(e,guild_id)=>{
-        if (guild_id==="home"){ //check if user is opening the "home"
+    ipcMain.on("requestOpenGuildContent", (e, guild_id) => {
+        if (guild_id === "home") { //check if user is opening the "home"
             e.sender.send("openHomeContent"); // if opening home, send required data for home
             return
         }
 
-        client.guilds.fetch(guild_id).then((guild)=>{
-            e.sender.send("openGuildContent",guild.id,guild.name);
+        client.guilds.fetch(guild_id).then((guild) => {
+            e.sender.send("openGuildContent", guild.id, guild.name);
             // Send back a signal with required data
 
 
@@ -43,10 +54,22 @@ function createWindow(){
 
     })
 
-    ipcMain.on("populateGuildChannel",(e,guildId)=> {
+
+    ipcMain.on("requestOpenChannelContent", (e, channelId) => {
+        client.channels.fetch(channelId).then((channel) => {
+            e.sender.send("openChannelContent", channel.id, channel.name)
+        })
+
+    })
+
+
+    // Populate content listeners
+
+
+    ipcMain.on("populateGuildChannel", (e, guildId) => {
         // console.log("populating...")
         // Loop through guild channels and add them tto sidebar
-        client.guilds.fetch(guildId).then((guild)=> {
+        client.guilds.fetch(guildId).then((guild) => {
             for (const c of guild.channels.cache) {
 
                 let channel = c[1]
@@ -59,15 +82,39 @@ function createWindow(){
                 if (channel.deleted === true) {
                     continue
                 }
-                e.sender.send("addGuildChannel", guild.id, channel.id, channel.name)
+                e.sender.send("addGuildChannel", channel.id, channel.name)
             }
         })
     })
 
+    ipcMain.on("populateChannelChat", (e, channelId) => {
+        client.channels.fetch(channelId).then((channel) => {
+            if (channel.type != "text") {
+                console.error("Error while populating channel chat: channel not of text type")
+                return
+            }
+
+            channel.messages.fetch({limit: 50}).then((messages) => {
+                messages.forEach((msg, msgId) => {
+                    // note: oldest text comes first.
+                    let moment_date = moment(msg.date)
+                    // Format date to string
+                    e.sender.send("addChatMessage", msg.content,
+                                  {id: msg.author.id, name: msg.author.username, avatarURL: msg.author.avatarURL()},
+                                  {date:msg.createdAt,string:moment_date.calendar(),timestamp:moment_date.unix()});
+                })
+            })
+
+
+        })
+
+    })
+
+
 }
 
 
-app.whenReady().then(()=>{
+app.whenReady().then(() => {
     createWindow()
 
     app.on('activate', () => {
@@ -90,9 +137,9 @@ app.on("before-quit", () => {
 });
 
 
-
 function login_request_callback() {
-    win.webContents.send("loggedin",true); //prevent user from clicking button again
+    win.webContents.send("loggedin", true); //prevent user from clicking button again
+
 
     prompt({
         title: "Discord Bot Login",
@@ -114,8 +161,10 @@ function login_request_callback() {
     );
 }
 
-function discordLogin(token){
-    if (token===null){return}
+function discordLogin(token) {
+    if (token === null) {
+        return
+    }
 
     client.login(token).catch((r) => {
 
@@ -125,11 +174,11 @@ function discordLogin(token){
 }
 
 
-client.on('ready', ()=>{
-    win.webContents.send("botready",client.user.username,client.user.avatarURL(),client.user.tag)
+client.on('ready', () => {
+    win.webContents.send("botready", client.user.username, client.user.avatarURL(), client.user.tag)
 
     for (const g of client.guilds.cache) {
-        win.webContents.send("addGuild",g[1].name,g[1].id,g[1].iconURL())
+        win.webContents.send("addGuild", g[1].name, g[1].id, g[1].iconURL())
     }
 
 })
--- preload.js
+++ preload.js
@@ -1,8 +1,4 @@
 const { ipcRenderer } = require('electron')
-const Discord = require("discord.js")
-
-
-
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -23,6 +19,15 @@ window.addEventListener('DOMContentLoaded', () => {
         document.getElementById("profile-area-user-id").textContent = tag;
     })
 
+    // Add click event listener for home
+    document.getElementById("guild-home").addEventListener("click", () => {
+        ipcRenderer.send("requestOpenGuildContent", "home")
+        // Send signal to main process, main process thens sends back another signal with required data
+    });
+
+
+
+    // Open Content Listeners
 
     ipcRenderer.on("openHomeContent",()=>{
         // When in home, no member list, so hide it
@@ -48,11 +53,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
     })
 
-    // Add click event listener for home
-    document.getElementById("guild-home").addEventListener("click", () => {
-        ipcRenderer.send("requestOpenGuildContent", "home")
-        // Send signal to main process, main process thens sends back another signal with required data
-    });
+    ipcRenderer.on("openChannelContent",(e,channelId,channelName)=>{
+        document.getElementById("chat-title").textContent = "# "+channelName
+        // Clear chat elements
+        document.getElementById("messages-container").innerHTML=""
+        ipcRenderer.send("populateChannelChat",channelId)
+    })
+
+
+    // addContent Listeners
 
     ipcRenderer.on("addGuild",(e,name,guildId,imageUrl)=>{
         let guildlist = document.getElementById("guildlist")
@@ -77,18 +86,94 @@ window.addEventListener('DOMContentLoaded', () => {
         guildlist.append(guildItemContainer)
     })
 
-    ipcRenderer.on("addGuildChannel", (e, guildId, channelId, channelName) => {
+    ipcRenderer.on("addGuildChannel", (e, channelId, channelName) => {
         let channel_container = document.getElementById("sidebar-channel-container")
         let channel_block = document.createElement("div")
         let channel_label = document.createElement("label")
-        channel_label.textContent="#"+channelName
+        channel_label.textContent="# "+channelName
         channel_label.style.fontSize="inherit"
         channel_block.append(channel_label)
         channel_block.className = "channel-container-item row"
-        channel_block.dataset.guildId=guildId
         channel_block.dataset.channelId=channelId
+
+        // Add event handler
+        channel_block.addEventListener("click",()=>{
+            ipcRenderer.send("requestOpenChannelContent",channelId)
+        })
+
         channel_container.append(channel_block)
     });
+
+
+    ipcRenderer.on("addChatMessage",(e,text,authorData,createdTimeData)=>{
+        let message_container = document.getElementById("messages-container")
+
+        let message_item;
+        let message_content;
+        let message_text_container;
+
+        // Check if msg container has any children   | Get prev message item author Id
+        if (message_container.childElementCount > 0 &&
+            message_container.firstChild.dataset.authorId===authorData.id &&
+            (createdTimeData.timestamp-message_container.firstChild.dataset.timestamp) <=300){
+            // if same author, set the new message-text to same msg-text-container
+
+            message_item = message_container.firstChild
+            message_content = message_item.lastChild
+            message_text_container = message_content.lastChild
+
+        }
+        else{
+            // else construct new message item
+
+            message_item = document.createElement("div")
+            message_item.className = "message-container-item row"
+            message_item.dataset.authorId = authorData.id
+            message_item.dataset.timestamp = createdTimeData.timestamp
+
+            let message_avatar = document.createElement("input")
+            message_avatar.className = "message-item-profile"
+            message_avatar.type="image"
+            message_avatar.src = authorData.avatarURL
+            message_item.append(message_avatar)
+
+            message_content = document.createElement("div")
+            message_content.className="message-item-content column"
+
+            let message_content_header = document.createElement("div")
+            message_content_header.className="row message-content-header"
+
+            let message_header_authorname = document.createElement("div")
+            message_header_authorname.textContent=authorData.name
+
+            message_content_header.append(message_header_authorname)
+
+            let message_header_timestamp = document.createElement("div")
+            message_header_timestamp.className="msg-header-timestamp"
+            message_header_timestamp.textContent = createdTimeData.string
+            message_content_header.append(message_header_timestamp)
+
+            message_text_container = document.createElement("div")
+            message_text_container.className="message-text-container column"
+
+            message_content.append(message_content_header)
+            message_content.append(message_text_container)
+
+            message_item.append(message_content)
+            message_container.insertBefore(message_item,message_container.firstChild)
+
+        }
+
+        let message_text = document.createElement("div")
+        message_text.className="message-text row"
+        message_text.textContent = text
+
+        // appends
+
+        message_text_container.insertBefore(message_text,message_text_container.firstChild)
+
+
+    })
 
 })
 
