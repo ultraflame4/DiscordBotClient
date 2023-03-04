3cc114e96ffc47019e6f7f0b69309953d3f55eb1
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-04 21:46:54 +0800

    Added  ui code for bot login

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,28 @@ import {BotHomeChannels, BotHomeGuild} from "./utils";
 import BotHomeContent from "./components/BotHomeContent";
 
 
-export default function App () {
-    const [guildList,setGuildList] = useState<SimplifiedGuildInfo[]>([])
-    const [openedGuild,setOpenedGuild] = useState<SimplifiedGuildInfo>(BotHomeGuild)
-    const [currentChannel,setCurrentChannel] = useState<SimplifiedChannelInfo|null>(null)
+export default function App() {
+    const [guildList, setGuildList] = useState<SimplifiedGuildInfo[]>([])
+    const [openedGuild, setOpenedGuild] = useState<SimplifiedGuildInfo>(BotHomeGuild)
+    const [currentChannel, setCurrentChannel] = useState<SimplifiedChannelInfo | null>(null)
 
     function UpdateGuildList() {
-        if (!discordApi.ready){
+        if (!discordApi.ready) {
             setGuildList([])
             return
         }
         discordApi.getGuildList().then(guilds => {
             setGuildList(guilds)
+        })
+    }
+
+    function LoginApi(token: string) {
+        discordApi.login(token).then((success) => {
+            if (success) {
+                UpdateGuildList()
+                return
+            }
+            alert("Login failed")
         })
     }
 
@@ -28,17 +38,19 @@ export default function App () {
     }, [discordApi.ready])
 
 
-
     return (
         <div className="App">
             <div className={"guild-list"}>
-                <GuildList guilds={guildList} onSelectGuild={(g)=>{setOpenedGuild(g)}} selectedGuild={openedGuild}/>
+                <GuildList guilds={guildList} onSelectGuild={(g) => {
+                    setOpenedGuild(g)
+                }} selectedGuild={openedGuild}/>
             </div>
             <div className={"channels-header"}>
-                {openedGuild?.name??"No guild open"}
+                {openedGuild?.name ?? "No guild open"}
             </div>
             <div className={"channels-list"}>
-                <ChannelList guildId={openedGuild.id} onSetCurrentChannel={setCurrentChannel} currentChannel={currentChannel}/>
+                <ChannelList guildId={openedGuild.id} onSetCurrentChannel={setCurrentChannel}
+                             currentChannel={currentChannel}/>
             </div>
             <div className={"user-info"}>
 
@@ -48,7 +60,8 @@ export default function App () {
             </div>
 
             <div className={"content"}>
-                {currentChannel?.type === "bot-home" && <BotHomeContent channel_id={currentChannel.id}/>}
+                {currentChannel?.type === "bot-home" &&
+                    <BotHomeContent channel_id={currentChannel.id} on_requestLogin={LoginApi}/>}
             </div>
         </div>
     )
--- src/components/BotHomeContent.tsx
+++ src/components/BotHomeContent.tsx
@@ -1,23 +1,32 @@
 import {defineComponent} from "../utils";
 import classes from "./BotHomeContent.module.css";
 import {discordApi} from "../api";
+import {useRef} from "react";
 
 
-const BotHomeAuth = defineComponent(props => {
-    return <div className={classes.botHomeContent}>
-        <label htmlFor={"token"}>Bot Key</label>
-        <input type={"password"} placeholder={"Enter Api Key"} id={"token"} disabled={discordApi.ready}/>
-        <button>Login</button>
-    </div>
+interface BotHomeAuthProps{
+    onLogin: (token: string) => void
+}
+
+const BotHomeAuth = defineComponent<BotHomeAuthProps>(props => {
+    const tokenInp = useRef<HTMLInputElement>(null)
+
+
+    return <>
+        <label htmlFor={"token"}>Token</label>
+        <input type={"password"} placeholder={"Enter Bot Api token"} id={"token"} disabled={discordApi.ready} ref={tokenInp}/>
+        <button onClick={e=>props.onLogin(tokenInp.current?.value??"")}>Login</button>
+    </>
 })
 
 interface BotHomeProps{
-    channel_id: string
+    channel_id: string,
+    on_requestLogin: (token: string) => void
 }
 
 export default defineComponent<BotHomeProps>(props => {
 
     return <div className={classes.botHomeContent}>
-        {props.channel_id === "bot-account" && <BotHomeAuth/>}
+        {props.channel_id === "bot-account" && <BotHomeAuth onLogin={props.on_requestLogin}/>}
     </div>
 })
