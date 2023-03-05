5bc7169604fd9d624654c2c13cf6691e2ef6cc88
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-05 13:08:51 +0800

    pretty much a clean state

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,66 @@
+import { rmSync } from 'node:fs'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
+import electron from 'vite-plugin-electron'
+import renderer from 'vite-plugin-electron-renderer'
+import pkg from './package.json'
+import vuetify from "vite-plugin-vuetify";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    port: 5173
-  },
+export default defineConfig(({ command }) => {
+  rmSync('dist-electron', { recursive: true, force: true })
 
+  const isServe = command === 'serve'
+  const isBuild = command === 'build'
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
+
+  return {
+    plugins: [
+      vue(),
+      electron([
+        {
+          // Main-Process entry file of the Electron App.
+          entry: 'electron/main.ts',
+
+          vite: {
+            build: {
+              sourcemap,
+              minify: isBuild,
+              outDir: 'dist-electron',
+              rollupOptions: {
+                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
+              },
+            },
+          },
+        },
+        {
+          entry: 'electron/preload/index.ts',
+          onstart(options) {
+            // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
+            // instead of restarting the entire Electron App.
+            options.reload()
+          },
+          vite: {
+            build: {
+              sourcemap: sourcemap ? 'inline' : undefined, // #332
+              minify: isBuild,
+              outDir: 'dist-electron/preload',
+              rollupOptions: {
+                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
+              },
+            },
+          },
+        }
+      ])
+    ],
+    server: process.env.VSCODE_DEBUG && (() => {
+      const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
+      return {
+        host: url.hostname,
+        port: +url.port,
+      }
+    })(),
+    clearScreen: false,
+  }
 })
